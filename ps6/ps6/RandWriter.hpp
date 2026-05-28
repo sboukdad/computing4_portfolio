8cd72e46107ba2841b7b47eee1e06ccc0fddd94c
sboukdad <salmaboukdad@gmail.com>
2026-05-28 19:52:19 -0400

    Initial push

---
+++ ps6/ps6/RandWriter.hpp
@@ -0,0 +1,45 @@
+/***********************************************************
+ * Copyright Salma Boukdad
+ * 
+ * @Filename: RandWriter.hpp
+ * @Cource: COMP.2040.203
+ * @Date: 5/25/26
+ * @Description: 
+***********************************************************/
+
+#include <iostream>
+#include <string>
+#include <map>
+
+
+class RandWriter {
+ public:
+    // Create a Markov model of order k from given text
+    // Assume that text has length at least k.
+    RandWriter(const std::string& str, size_t k);
+
+    size_t orderK() const; // Order k of Markov model
+
+    // Number of occurences of kgram in text
+    // Throw an exception if kgram is not length k
+    int freq(const std::string& kgram) const;
+    // Number of times that character c follows kgram
+    // if order=0, return num of times that char c appears
+    // (throw an exception if kgram is not of length k)
+    int freq(const std::string& kgram, char c) const;
+
+    // Random character following given kgram
+    // (throw an exception if kgram is not of length k)
+    // (throw an exception if no such kgram)
+    char kRand(const std::string& kgram);
+    // Generate a string of length L characters by simulating a trajectory
+    // through the corresponding Markov chain. The first k characters of
+    // the newly generated string should be the argument kgram.
+    // Throw an exception if kgram is not of length k.
+    // Assume that L is at least k
+    std::string generate(const std::string& kgram, size_t l);
+
+   
+ private:
+    // Private member variables go here
+};
